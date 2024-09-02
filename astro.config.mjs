b0584d2b474ee1bdf10770b4a188e5ce92e3660b
Tapas Jena <tapas.friends@gmail.com>
2024-09-02 23:44:31 +0800

    fix: build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,10 +45,10 @@ export default defineConfig({
           label: 'Guides',
           items: [
             // Each item here is one entry in the navigation menu.
-            {
-              label: 'Example Guide',
-              slug: 'guides/example'
-            }
+            // {
+            //   label: 'Example Guide',
+            //   slug: 'guides/example'
+            // }
           ]
         },
         {
